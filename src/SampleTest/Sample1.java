ae66d8b67f2426a0d13f8839d314144bba921ea0
Mayank_234 <mayanksengar1604@gmail.com>
2023-09-25 11:48:54 +0530

    First Commit

---
+++ src/SampleTest/Sample1.java
@@ -0,0 +1,8 @@
+package SampleTest;
+
+public class Sample1 {
+	public static void main(String[] args) {
+		System.out.println("This is the First Statement");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mayanksengar
+ *
+ */
+module GitHubBench {
+}

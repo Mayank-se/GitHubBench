07a9ae97347bdcde9aa6c2b6658d6bda8a41f9f1
Mayank_234 <mayanksengar1604@gmail.com>
2023-09-25 12:06:48 +0530

    change in First Commit

--- src/SampleTest/Sample1.java
+++ src/SampleTest/Sample1.java
@@ -3,6 +3,7 @@ package SampleTest;
 public class Sample1 {
 	public static void main(String[] args) {
 		System.out.println("This is the First Statement");
+		
+		System.out.println("This is the Second Statement");
 	}
-
 }

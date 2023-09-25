672322cdf9ab67a6ed9c0fba8e42f047e9a52573
Mayank_234 <mayanksengar1604@gmail.com>
2023-09-25 12:46:43 +0530

    Fourth Commit

--- src/SampleTest/Sample2.java
+++ src/SampleTest/Sample2.java
@@ -1,9 +1,9 @@
 package SampleTest;
 
 public class Sample2 {
-
-	public static void main(String[] args) {
-		System.out.println("This is the Third Statement");
-	}
-
+public static void main(String[] args) {
+	System.out.println("This is the Third Statement");
+	
+	System.out.println("This is the Four Statement");
+}
 }
